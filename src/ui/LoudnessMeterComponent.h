4a863e611da0dcf04ccb68db753da8a14bfed8bc
Kay Schäfer <148460726+Neuroklast@users.noreply.github.com>
2025-06-23 15:41:44 +0200

    Add OpenGL loudness meter with scalable context menu

--- src/ui/LoudnessMeterComponent.h
+++ src/ui/LoudnessMeterComponent.h
@@ -3,23 +3,38 @@
 #include <JuceHeader.h>
 #include "../core/PluginProcessor.h"
 
-class LoudnessMeterComponent : public juce::Component, private juce::Timer
+class LoudnessMeterComponent : public juce::Component,
+                               private juce::OpenGLRenderer,
+                               private juce::Timer
 {
 public:
     explicit LoudnessMeterComponent(NeuroCoreAudioProcessor& proc);
-    ~LoudnessMeterComponent() override = default;
+    ~LoudnessMeterComponent() override;
 
     void paint(juce::Graphics& g) override;
     void resized() override {}
+    void mouseDown(const juce::MouseEvent& e) override;
 
 private:
     void timerCallback() override;
     void drawLed(juce::Graphics& g, juce::Rectangle<float> area, bool on);
+    void newOpenGLContextCreated() override;
+    void renderOpenGL() override;
+    void openGLContextClosing() override {}
+
+    enum class Scale { dBFS, LUFS, KSystem };
+    struct ScaleInfo { const char* name; float minDb; float maxDb; float step; };
+    ScaleInfo currentScaleInfo() const noexcept;
+    float valueToY(float db, juce::Rectangle<float> area) const noexcept;
+    void showContextMenu();
 
     NeuroCoreAudioProcessor& processor;
     float loudness { -100.0f };
     bool  limiter { false };
     bool  blink   { false };
     int   blinkCount { 0 };
+
+    Scale scale { Scale::dBFS };
+    juce::OpenGLContext openGLContext;
 };
 
