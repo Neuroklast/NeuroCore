0bb040e9481e14446325699891c6421d305ce4c0
Kay Schäfer <148460726+Neuroklast@users.noreply.github.com>
2025-06-23 14:25:51 +0200

    Remove redundant GL headers

--- src/ui/WaveformDisplayComponent.cpp
+++ src/ui/WaveformDisplayComponent.cpp
@@ -5,15 +5,6 @@
 #include <deque>
 #include "../dsp/DSPUtils.h"
 #include <juce_opengl/juce_opengl.h>
-#if JUCE_WINDOWS
-#include <GL/gl.h>
-#include <GL/glu.h>
-#elif JUCE_MAC
-#include <OpenGL/gl.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#endif
 
 WaveformDisplayComponent::WaveformDisplayComponent(NeuroCoreAudioProcessor& proc, Type t)
     : processor(proc), type(t)
